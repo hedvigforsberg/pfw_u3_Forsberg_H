92337250bea758ebe53ada9b31506b82829a93ed
hedvigforsberg <hedvigforsberg@gmail.com>
2022-03-23 14:05:53 +0100

    Restarted with a new method of writing the code

--- students.js
+++ students.js
@@ -1,5 +1,9 @@
 "use strict";
 
+// Global Variables
+
+let input = document.getElementById("searchbox");
+
 // Function to clear input-box on each reload.
 window.onload = function () {
   document.getElementById("searchbox").value = "";
@@ -7,23 +11,46 @@ window.onload = function () {
 
 // Function to search in the input-box for students by their last name from the letters you type.
 
-function searchForStudent() {
-  let input = document.getElementById("searchbox");
-  input.value.toLowerCase();
+// function searchForStudent() {
 
-  let students = findStudentsByLastName(input);
-  renderStudents(students);
-}
+// }
 
 // Function to find students by their last name.
 
-function findStudentsByLastName(name) {
-  return DATABASE.students.filter(student => {
-    return student.lastName.toLowerCase().includes(name);
-  });
+function findStudentsByLastName() {
+  let student = DATABASE.students
+    .filter((student) => student.lastName.toLowerCase().includes(input.value))
+    .map((student) => student.firstName + " " + student.lastName);
+  return student;
 }
+
 // Function to render students onto the webpage.
 
-function renderStudents() {
+function createStudent(student) {
+  let div = document.createElement("div");
+  div.classList.add("students");
+  let studentsDiv = document.getElementById("students");
+  studentsDiv.appendChild(div);
 
+  div.innerHTML = `${student}`;
+};
+
+function createHTML(students) {
+  students.forEach(student => {
+    createStudent(student);
+  })
 }
+
+// Event Listener
+
+input.addEventListener("keyup", function () {
+  let student = findStudentsByLastName();
+  let studentsDiv = document.getElementById("students");
+
+  studentsDiv.innerHTML = "";
+  createHTML(student);
+
+  if (input.value == 0) {
+    studentsDiv.innerHTML = "";
+  }
+});
