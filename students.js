eb1f5879988fd64f2a3924e2f03fa2c14c30858d
hedvigforsberg <hedvigforsberg@gmail.com>
2022-03-23 11:06:04 +0100

    Started declaring some basic functions

--- students.js
+++ students.js
@@ -1,1 +1,29 @@
 "use strict";
+
+
+// Function to clear input-box on each reload.
+window.onload = function () {
+  document.getElementById("searchbox").value = "";
+};
+
+// Function to search in the input-box for students by their last name from the letters you type.
+
+function searchForStudent () {
+  let input = document.getElementById("searchbox");
+  input.value.toLowerCase();
+
+  let students = findStudentsByLastName(input);
+  renderStudents(students);
+}
+
+// Function to find students by their last name.
+
+function findStudentsByLastName() {
+
+}
+// Function to render students onto the webpage.
+
+function renderStudents () {
+  
+}
+
