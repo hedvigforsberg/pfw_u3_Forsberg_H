f5d9d757f41b8c7a1edc8ce704e5ab86f7dee0a0
hedvigforsberg <hedvigforsberg@gmail.com>
2022-03-27 09:55:21 +0200

    More working with functions in order to render content on webpage

--- courses.js
+++ courses.js
@@ -1,1 +1,48 @@
 "use strict";
+
+let input = document.getElementById("searchbox");
+
+window.onload = function () {
+    document.getElementById("searchbox").value = "";
+  };
+  
+function findCourseByTitle() {
+  let course = DATABASE.courses
+    .filter((course) =>
+      course.title.toLowerCase().includes(input.value.toLowerCase())
+    )
+    .map((course) => course.title + " " + course.totalCredits);
+
+    course.sort();
+
+    return course;
+}
+
+function createCourse(course) {
+    let div = document.createElement("div");
+
+    let coursesDiv = document.getElementById("courses");
+    coursesDiv.appendChild(div);
+
+    div.innerHTML = `
+        <div>${course}</div>
+    `;
+}
+
+function renderCourses(courses) {
+    courses.forEach((course) => {
+        createCourse(course);
+    })
+}
+
+input.addEventListener("keyup", function () {
+    let course = findCourseByTitle();
+    let coursesDiv = document.getElementById("courses");
+
+    coursesDiv.innerHTML = "";
+    renderCourses(course);
+    
+    if (input.value == 0) {
+        coursesDiv.innerHTML = "";
+    }
+})
--- students.js
+++ students.js
@@ -10,50 +10,89 @@ let input = document.getElementById("searchbox");
 // FUNCTIONS
 
 // Function to clear input-box on each page-refresh.
-window.onload = function () {
-  document.getElementById("searchbox").value = "";
-};
 
 // Function to find students in the database by their last name.
 
 function findStudentsByLastName() {
-  let foundStudent = DATABASE.students
-    .filter((student) =>
-      student.lastName.toLowerCase().includes(input.value.toLowerCase()))
-    .map((student) => student.firstName + " " + student.lastName);
-  
-  foundStudent.sort();
-  return foundStudent;
-};
+  let student = DATABASE.students.filter((student) =>
+    student.lastName.toLowerCase().includes(input.value.toLowerCase())
+  );
+  // .map((student) => student.firstName + " " + student.lastName);
+
+  student.sort();
+
+  return student;
+}
 
 // Function to create a student.
 
-function createStudent(student) {
+function renderStudent(student) {
   let div = document.createElement("div");
 
   let studentsDiv = document.getElementById("students");
   studentsDiv.appendChild(div);
+  
 
+  let credits = totalCredits(student);
+
+  let foundCourses = findCourseById(student);
+  
+  div.classList.add("student");
   div.innerHTML = `
-    <div>${student}</div>
-    <div>(Total: ${"totalCredits"} credits)</div>
+    <div>${student.firstName} ${student.lastName} (Total: ${credits} credits)</div>
     <div id=courses>Courses:</div>
   `;
-};
+
+  for (let i = 0; i < foundCourses.length; i++) {
+    let foundCourse = foundCourses[i];
+
+    let courseDiv = document.createElement("div");
+    courseDiv.classList.add("course");
+    div.appendChild(courseDiv);
+    
+    courseDiv.innerText =
+      foundCourse.title + ", " + "of" + " " + foundCourse.totalCredits + " " + "credits";
+    
+  //   let courseInfoDiv = document.createElement("div");
+  //   courseDiv.appendChild(courseInfoDiv);
+  //   courseInfoDiv.innerText = 
+  //     foundCourse.started.semester;
+   }
+}
 
 // Function to loop through each student to add the HTML onto the webpage.
 
 function renderStudents(students) {
   students.forEach((student) => {
-    createStudent(student);
+    renderStudent(student);
   });
-};
+}
 
+function totalCredits(student) {
+  let credit = [];
+  for (let course of student.courses) {
+    credit.push(course.passedCredits);
+  }
+  let totalSum = 0;
+  for (let i = 0; i < credit.length; i++) {
+    totalSum += credit[i];
+  }
+  return totalSum;
+}
 // Function to find courses
 
-function findCoursesForStudent () {
-  let student = foundStudent;
+function findCourseById(student) {
+  let foundCourses = [];
+  for (let i = 0; i < student.courses.length; i++) {
+    foundCourses.push(
+      DATABASE.courses.find((course) => {
+        return course.courseId == student.courses[i].courseId;
+      })
+    );
+  }
+  return foundCourses;
 }
+
 // EVENT LISTENERS
 
 // Event-listener to run the functions to filter through students each time a key is pressed.
